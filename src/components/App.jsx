d5d39a582c87d9301154a3481f135ca1880b9ae3
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-04-29 23:02:43 +0300

    add folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import { Profile } from './Profile';
-import { Statistics } from './Statistics';
-import { FriendList } from './FriendList';
-import { TransactionHistory } from './TransactionHistory';
+import { Profile } from '../components/Profile/Profile';
+import { Statistics } from '../components/Statistics/Statistics';
+import { FriendList } from '../components/FriendList/FriendList';
+import { TransactionHistory } from '../components/TransactionHistory/TransactionHistory';
 
 import user from '../data/user.json';
 import data from '../data/data.json';
